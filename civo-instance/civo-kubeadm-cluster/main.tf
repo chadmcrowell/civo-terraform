9bf3e8634a3b29c3efff327379eb56a058ee0117
ChadMCrowell <chadmcrowell@gmail.com>
2024-06-03 12:12:23 -0500

    added instances

--- civo-instance/civo-kubeadm-cluster/main.tf
+++ civo-instance/civo-kubeadm-cluster/main.tf
@@ -152,6 +152,27 @@ resource "civo_instance" "k8s_controlplane" {
     sshkey_id = "457f9017-e1a8-486a-9bdb-e99486842011"
     firewall_id = civo_firewall.controlplane_firewall.id
     network_id = civo_network.instance_net.id
+    
+    script = <<-EOF
+      #!/bin/bash
+      sudo apt-get update
+      sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+      sudo mkdir -p -m 755 /etc/apt/keyrings
+      sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+      echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+      sudo apt-get update
+      sudo apt-get install -y kubelet=1.30.1-1.1 kubeadm=1.30.1-1.1 kubectl=1.30.1-1.1
+      sudo apt-mark hold kubelet kubeadm kubectl
+      sudo apt-get install -y containerd
+      sudo mkdir -p /etc/containerd
+      containerd config default | sudo tee /etc/containerd/config.toml
+      sudo systemctl restart containerd
+      sudo sysctl -w net.ipv4.ip_forward=1
+      sudo sed -i '/^#net\.ipv4\.ip_forward=1/s/^#//' /etc/sysctl.conf
+      sudo sysctl -p
+      sudo wget -P $HOME "https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml"
+      sudo wget -P $HOME "https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml" 
+    EOF
 }
 
 resource "civo_instance" "k8s_worker" {
@@ -161,4 +182,25 @@ resource "civo_instance" "k8s_worker" {
     sshkey_id = "457f9017-e1a8-486a-9bdb-e99486842011"
     firewall_id = civo_firewall.workernode_firewall.id
     network_id = civo_network.instance_net.id
+    
+    script = <<-EOF
+      #!/bin/bash
+      sudo apt-get update
+      sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+      sudo mkdir -p -m 755 /etc/apt/keyrings
+      sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+      echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+      sudo apt-get update
+      sudo apt-get install -y kubelet=1.30.1-1.1 kubeadm=1.30.1-1.1 kubectl=1.30.1-1.1
+      sudo apt-mark hold kubelet kubeadm kubectl
+      sudo apt-get install -y containerd
+      sudo mkdir -p /etc/containerd
+      containerd config default | sudo tee /etc/containerd/config.toml
+      sudo systemctl restart containerd
+      sudo sysctl -w net.ipv4.ip_forward=1
+      sudo sed -i '/^#net\.ipv4\.ip_forward=1/s/^#//' /etc/sysctl.conf
+      sudo sysctl -p
+      sudo wget -P $HOME "https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml"
+      sudo wget -P $HOME "https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml" 
+    EOF
 }
---
+++ civo-instance/civo-ubuntu-instance/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    civo = {
+      source = "civo/civo"
+      version = "1.0.41"
+    }
+  }
+}
+
+provider "civo" {
+    token = var.civo_api_key
+    region = var.region
+}
+
+resource "civo_network" "jumphost_net" {
+  label = "jumphost-net"
+}
+
+resource "civo_firewall" "jump_firewall" {
+  name                 = "jump-firewall"
+  network_id           = civo_network.jumphost_net.id
+  create_default_rules = false
+  ingress_rule {
+    label      = "ssh"
+    protocol   = "tcp"
+    port_range = "22"
+    cidr       = [var.local_cidr]
+    action     = "allow"
+  }
+  
+  ingress_rule {
+    label      = "http"
+    protocol   = "tcp"
+    port_range = "80"
+    cidr       = ["0.0.0.0/0"]
+    action     = "allow"
+  }
+  
+  ingress_rule {
+    label      = "https"
+    protocol   = "tcp"
+    port_range = "443"
+    cidr       = ["0.0.0.0/0"]
+    action     = "allow"
+  }
+
+  egress_rule {
+    label      = "all"
+    protocol   = "tcp"
+    port_range = "1-65535"
+    cidr       = ["0.0.0.0/0"]
+    action     = "allow"
+  }
+}
+
+data "civo_disk_image" "ubuntu" {
+   filter {
+        key = "name"
+        values = ["ubuntu-focal"]
+   }
+}
+
+resource "civo_instance" "vm" {
+    hostname = "vm.xyz"
+    disk_image = element(data.civo_disk_image.ubuntu.diskimages, 0).id
+    size = "g3.xlarge"
+    sshkey_id = var.ssh_key_id
+    
+    script = local.startup_script
+}
+
